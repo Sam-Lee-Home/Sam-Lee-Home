2e524119ac74611c3aee66e388dedaf34e28a582
Sam Lee <kingyanlee@yahoo.com>
2026-09-21 13:55:05 -0400

    fix error.  browser display failed on everything other than the home page.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sam-lee-home.github.io',
+  base: '/Sam-Lee-Home',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
